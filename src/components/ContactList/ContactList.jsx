a7f6a8e4b6cfa0874aed154fbacf52dbc2a25ae3
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-03-30 13:27:29 +0100

    contactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,15 +1,18 @@
-import css from './ContactList.module.css'
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from 'store/contactSlice';
+import css from './ContactList.module.css';
 
-export const ContactList = ({ contacts, onDeleteContact }) => (
+export const ContactList = ({ contacts }) => {
+  const dispatch = useDispatch();
+
+  const handleDeleteContact = id => {
+    dispatch(deleteContact(id));
+  };
+
+  return (
     <ul className={css.wrapper}>
-      {contacts.map((contact) => (
-        <li className={css.contact} key={contact.id}>
-          {contact.name}: {contact.number}
-          <button className={css.button} type="button" onClick={() => onDeleteContact(contact.id)}>
-            Delete
-          </button>
-        </li>
-      ))}
+      {/* List Items */}
     </ul>
   );
- 
+};
