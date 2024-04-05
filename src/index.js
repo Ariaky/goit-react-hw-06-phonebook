70dfa4ee0fbb10a55c902434ab24ece04b7aa7f9
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-04-05 22:17:18 +0200

    persistor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { store } from './store/store'; // імпортуйте ваш створений store
+import { store, persistor } from './store/store'; // імпортуйте ваш створений store
 import { App } from './components/App';
 import './index.css';
 import { PersistGate } from 'redux-persist/integration/react';
