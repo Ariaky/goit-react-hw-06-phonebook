be43fe7fa8b70f07781bf6565e1927806ff38b55
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-04-05 22:29:24 +0200

    commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import contactReducer from './contactSlice';
+import { contactReducer } from './contactSlice';
 import { 
   persistStore,
   FLUSH,
