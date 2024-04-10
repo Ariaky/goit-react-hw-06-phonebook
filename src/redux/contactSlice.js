65571a235430c3e274e063c8585b9ae5ce465a4c
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-04-10 22:41:53 +0200

    commit

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const initialState = {
   contacts: [],
