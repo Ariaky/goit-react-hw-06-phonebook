6675de5db97477d5b7efefb8f2ccd6ab3f2c8d35
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-04-21 22:38:52 +0200

    commit

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -10,7 +10,16 @@ const ContactList = () => {
   const dispatch = useDispatch();
   return (
     <ul className={css.wrapper}>
-      
+      {contacts.map(({ id, name, number }) => 
+      (
+        <li key={id}>
+          {name}: {number}{' '}
+          <button type="button" onClick={() => dispatch(deleteContact(id))}>
+            Delete
+          </button>
+        </li>
+      )
+      )}
     </ul>
   )
 }
