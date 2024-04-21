ab17e44b1f522f05e35b53430a6becacb1886a58
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-04-21 10:46:05 +0200

    commit

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,14 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 export const getContacts = state => state.contacts.list;
 
 export const getFilterValue = state => state.filter;
 
+export const selectContact = createSelector(
+    [getContacts, getFilterValue],
+    (list,filter) => {
+        return list.filter(list =>
+            list.name.toLowerCase().includes(filter.toLowerCase())
+        );
+    }
+)
