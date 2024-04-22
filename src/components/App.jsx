bc6d9cbd2e95bc5d0752b727222893dc45a53ebf
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-04-22 18:13:09 +0200

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,9 @@
-/*import React from 'react';
-import { useSelector } from 'react-redux';*/
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import css from './App.module.css';
 
 export const App = () => {
- /* const contacts = useSelector(state => state.contacts.list);
-  const filter = useSelector(state => state.contacts.filter);*/
-
   return (
     <div className={css.wrapper}>
       <h1 className={css.title}>Phonebook</h1>
@@ -17,7 +12,6 @@ export const App = () => {
       <h2 className={css.title}>Contacts</h2>
       <Filter />
       <ContactList 
-      /*contacts={contacts}*/
        />
     </div>
   );
