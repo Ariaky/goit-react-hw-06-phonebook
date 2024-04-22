46e2a83314f5ca882d13f9dc0b2609101a1c1d69
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-04-22 17:57:56 +0200

    commit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,5 @@
 /*import React, { useState } from 'react';*/
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { nanoid } from 'nanoid';
 import { addContact } from '../../redux/contactSlice';
 import { useState } from 'react';
